536628e80a7c3c23198c0d81eb4af53757a3cbd3
dwjg111 <1879118732@qq.com>
2026-08-10 17:54:40 +0800

    第十四课字符串

---
+++ study0807/第一章/01入门.py
@@ -0,0 +1,20 @@
+# print(1)
+# print(0.1)
+# print(False)
+# print(True)
+# print("--")
+# print(None)
+# base = 10
+# incr = 20
+# print(base + incr, base - incr, base * incr, base / incr)
+
+a = 1
+b = 2
+c = 3
+# a, b, c = c, a, b
+d = a
+e = b
+a = c
+b = d
+c = e
+print(a, b, c)
