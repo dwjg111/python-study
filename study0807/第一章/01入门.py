1e5e6d0c077e1544a8989907c9cba4147baf1480
dwjg111 <1879118732@qq.com>
2026-09-07 17:05:42 +0800

    0907

--- study0807/第一章/01入门.py
+++ study0807/第一章/01入门.py
@@ -8,13 +8,40 @@
 # incr = 20
 # print(base + incr, base - incr, base * incr, base / incr)
 
-a = 1
-b = 2
-c = 3
-# a, b, c = c, a, b
-d = a
-e = b
-a = c
-b = d
-c = e
-print(a, b, c)
+# a = 1
+# b = 2
+# c = 3
+# # a, b, c = c, a, b
+# d = a
+# e = b
+# a = c
+# b = d
+# c = e
+# print(a, b, c)
+
+# name = "xiao"
+# age = 10
+# pro = "software engineering"
+# hobby = "python"
+# print("my name is %s, I am %s, study %s, my hobby is %s" % (name, age, pro, hobby))
+#
+# name = "xiao"
+# print(f"my name is {name}")
+#
+# name = input("请输入你的姓名：")
+# print(f"my name is {name}")
+# yu_e = 10000
+# secret = input("input your secret:")
+# print(f"确认你的密码{secret}")
+# money = input("input your money")
+# print(f"余额为{int(yu_e) - int(money)}")
+
+# a = input("请输入第一个数字：")
+# b = input("请输入第二个数字：")
+# print(f"{a}+{b}={int(a) + int(b)}")
+
+# print(0.5 - 0.4)
+
+number = int(input("请输入一个数字："))
+bool = number > 10 and number < 20
+print(bool)
